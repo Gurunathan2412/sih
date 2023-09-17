7d6dc5191ff910e2259f6db4a8523fff65da7dae
Guru <gurunathan2412@gmail.com>
2023-09-17 18:38:29 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {React} from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import {Typography,AppBar,Toolbar} from '@mui/material'
+import Header from './Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Typography variant='h1'>Hello</Typography>
+    </>
+  )
+}
+
+export default App
